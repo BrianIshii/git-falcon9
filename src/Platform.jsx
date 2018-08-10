eb35dd59dbb5c74deb36efecd508eb875abd12c6
Brian Ishii <brianlishii@gmail.com>
2018-08-09 22:29:40 -0700

    added exhaust

--- src/Platform.jsx
+++ src/Platform.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 export const exhaust = keyframes`
   0%{
-    box-shadow: 0px -20px 60px -5px rgba(255,255,255,0.7);
+    box-shadow: 0px -10px 60px -5px rgba(255,255,255,0.7);
   }
   60%{
-    box-shadow: 0px -30px 70px 5px white;
+    box-shadow: 0px -10px 70px 30px white;
   }
   100%{
-    box-shadow: 0px -10px 80px -10px rgba(255,255,255,0.5);
+    box-shadow: 0px -10px 80px 20px rgba(255,255,255,0.3);
   }
 `
 export const LaunchPad = styled.div `
@@ -18,12 +18,12 @@ export const LaunchPad = styled.div `
     display: ${props => (props.display ? 'block' : 'none')};
     height: 1px;
     width: 100px;
-    left: calc(90% - 50px);
+    left: calc(90% - 85px);
     right: 0;
     background: #444;
     animation-name: ${exhaust};
     animation-duration: 2s;
-    animation-delay: 0s;
+    animation-delay: 1s;
     animation-iteration-count: 1;
     will-change: box-shadow;
 `
@@ -39,7 +39,23 @@ export const smokeLeft = keyframes`
   100%{
     filter:blur(4px);
     opacity: 0.5;
-    left: -200px;
+    left: -100px;
+    height: 100px;
+    width: 100px;
+  }
+`
+export const smokeRight = keyframes`
+  0%{
+    filter:blur(4px);
+    opacity: 0.8;
+    height: 1px;
+    left: 20px;
+    width: 1px;
+  }
+  100%{
+    filter:blur(4px);
+    opacity: 0.5;
+    left: 100px;
     height: 100px;
     width: 100px;
   }
@@ -61,79 +77,68 @@ export const Smoke = styled.i`
   i:nth-child(1) {
     animation-name: ${smokeLeft};
     animation-iteration-count: infinite;
-    animation-duration: 4s;
+    animation-duration: 8s;
     animation-delay: 0.2s;
   }
   i:nth-child(2) {
-    animation-name: ${smokeLeft};
+    animation-name: ${smokeRight};
     animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 0.4s;
-  }
-  i:nth-child(3) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 0.6s;
-  }
-  i:nth-child(4) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 0.8s;
-  }
-  i:nth-child(5) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 1s;
-  }
-  i:nth-child(6) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 1.2s;
-  }
-  i:nth-child(7) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 1.4s;
-  }
-  i:nth-child(8) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 1.6s;
-  }
-  i:nth-child(9) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 1.8s;
-  }
-  i:nth-child(10) {
-    animation-name: ${smokeLeft};
-    animation-iteration-count: infinite;
-    animation-duration: 4s;
-    animation-delay: 2s;
-  }
+    animation-duration: 8s;
+    animation-delay: 0.2s;
+   }
+  // i:nth-child(3) {
+  //   animation-name: ${smokeLeft};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 0.6s;
+  // }
+  // i:nth-child(4) {
+  //   animation-name: ${smokeLeft};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 0.8s;
+  // }
+  // i:nth-child(5) {
+  //   animation-name: ${smokeLeft};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 1s;
+  // }
+  // i:nth-child(6) {
+  //   animation-name: ${smokeRight};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 0.2s;
+  // }
+  // i:nth-child(7) {
+  //   animation-name: ${smokeRight};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 0.4s;
+  // }
+  // i:nth-child(8) {
+  //   animation-name: ${smokeRight};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 0.6s;
+  // }
+  // i:nth-child(9) {
+  //   animation-name: ${smokeRight};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 0.8s;
+  // }
+  // i:nth-child(10) {
+  //   animation-name: ${smokeRight};
+  //   animation-iteration-count: infinite;
+  //   animation-duration: 4s;
+  //   animation-delay: 1s;
+  // }
 `
 
 const Platform = ({display, animationType}) => (
   <LaunchPad id="platform" display={display} animationType={animationType}>
-    <Smoke>
-      <i />
-      <i />
-      <i />
-      <i />
-      <i />
-      <i />
-      <i />
-      <i />
-      <i />
-      <i />
-    </Smoke>
+
   </LaunchPad>
 );
 
